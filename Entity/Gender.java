ab2d01f0d88ccfdc1c69e93d3b6ad75250dce227
magicalzw <wang3816@purdue.edu>
2021-02-28 08:22:10 -0500

    add the data structure of message, and some minor modifies to gender

--- Entity/Gender.java
+++ Entity/Gender.java
@@ -1,5 +1,22 @@
 import java.util.*;
 
-public enum Gender {
-    Male, Female, No_Tell;
+public class Gender {
+    private String gender;
+
+    public Gender(String gender) {
+        this.gender = gender;
+    }
+
+    //If no information about the gender, we will save it as no tell
+    public Gender() {
+        gender = "NO_TELL";
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getGender() {
+        return gender;
+    }
 }
