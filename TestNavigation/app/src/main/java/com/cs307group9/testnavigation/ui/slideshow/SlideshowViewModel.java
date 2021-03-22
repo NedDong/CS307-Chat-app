98b96494a507bc64ce0eeaf4043e2812e6e04a56
Tao Zhang <zhan3263@purdue.edu>
2021-03-22 09:01:32 +0800

    Test and Build Navigation UI

---
+++ TestNavigation/app/src/main/java/com/cs307group9/testnavigation/ui/slideshow/SlideshowViewModel.java
@@ -0,0 +1,19 @@
+package com.cs307group9.testnavigation.ui.slideshow;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class SlideshowViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public SlideshowViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is slideshow fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
