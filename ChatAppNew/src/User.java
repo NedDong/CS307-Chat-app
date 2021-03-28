426473b003b6b28b3ecd51ea1efeff3a216d4da2
magicalzw <wang3816@purdue.edu>
2021-03-28 08:26:46 -0400

    friend list and blocked list

--- ChatAppNew/src/User.java
+++ ChatAppNew/src/User.java
@@ -2,6 +2,9 @@
 import java.io.Serializable;
 import java.net.InetAddress;
 import java.net.Socket;
+import java.rmi.NoSuchObjectException;
+import java.util.ArrayList;
+import java.util.HashMap;
 
 public class User implements Serializable {
 
@@ -11,6 +14,8 @@ public class User implements Serializable {
     private transient String password;
     private transient InetAddress inetAddress;
     private transient int port;
+    private transient HashMap<Integer, User> friendList;
+    private transient HashMap<Integer, User> blockedList;
 
     public User(String username, int uid, InetAddress inetAddress, String password)
     {
@@ -19,6 +24,12 @@ public class User implements Serializable {
         this.inetAddress = inetAddress;
         this.password = password;
         this.uid = uid;
+    }
+
+    public User(String username, int uid, String password) {
+        this.username = username;
+        this.uid = uid;
+        this.password = password;
     }
 
     public void setUsername(String newUsername)
@@ -30,10 +41,15 @@ public class User implements Serializable {
         return uid;
     }
 
+    public HashMap<Integer, User> getFriendList() {return friendList;}
+
+    public HashMap<Integer, User> getBlockedList() {return blockedList;}
 
     public String getPassword() {
         return password;
     }
+
+    public void setPassword(String password) {this.password = password;}
 
     public String getUsername() {
         return username;
@@ -41,5 +57,30 @@ public class User implements Serializable {
 
     public InetAddress getInetAddress() {
         return inetAddress;
+    }
+
+    public void addFriend(User friend) {
+        friendList.put(friend.getUid(),friend);
+    }
+
+    public void removeFriend(User friend) throws NoSuchObjectException {
+        if(friendList.containsKey(friend.getUid())) {
+            friendList.remove(friend.getUid());
+        } else {
+            throw new NoSuchObjectException("No such user in friend list");
+        }
+    }
+
+    public void addBlockedList(User user) {
+        if(friendList.containsKey(user.getUid())) {
+            try {
+                removeFriend(user);
+            } catch (NoSuchObjectException e) {
+                e.printStackTrace();
+            }
+            blockedList.put(user.getUid(), user);
+        } else {
+            blockedList.put(user.getUid(), user);
+        }
     }
 }
