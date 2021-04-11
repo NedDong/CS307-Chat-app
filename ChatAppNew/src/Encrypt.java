eaddafdad31d6431a23a983c39e1b3ec5e06eae8
magicalzw <wang3816@purdue.edu>
2021-04-11 11:25:24 -0400

    encrypt

---
+++ ChatAppNew/src/Encrypt.java
@@ -0,0 +1,34 @@
+import javax.crypto.Cipher;
+import java.security.KeyPair;
+import java.security.KeyPairGenerator;
+import java.security.Signature;
+
+public class Encrypt {
+    public static void main(String args[]) throws Exception{
+        //Creating a Signature object
+        Signature sign = Signature.getInstance("SHA256withRSA");
+
+        //Creating KeyPair generator object
+        KeyPairGenerator keyPairGen = KeyPairGenerator.getInstance("RSA");
+
+        //Initializing the key pair generator
+        keyPairGen.initialize(2048);
+
+        //Generating the pair of keys
+        KeyPair pair = keyPairGen.generateKeyPair();
+
+        //Creating a Cipher object
+        Cipher cipher = Cipher.getInstance("RSA/ECB/PKCS1Padding");
+
+        //Initializing a Cipher object
+        cipher.init(Cipher.ENCRYPT_MODE, pair.getPublic());
+
+        //Adding data to the cipher
+        byte[] input = "Welcome to Tutorialspoint".getBytes();
+        cipher.update(input);
+
+        //encrypting the data
+        byte[] cipherText = cipher.doFinal();
+        System.out.println(new String(cipherText, "UTF8"));
+    }
+}
